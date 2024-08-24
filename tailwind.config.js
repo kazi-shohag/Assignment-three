7a4cc3e631be3c9c7ba85ddaf45ebbe620037bdf
kazi-shohag <kazishohag1166@gmail.com>
2024-08-24 14:10:09 +0600

    complet poject

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
     content: [],
     theme: {
-      extend: {},
+      extend: {
+        fontFamily:{
+          manrope: ['Manrope', 'sans-serif'],
+        },
+      },
     },
     plugins: [],
   }
